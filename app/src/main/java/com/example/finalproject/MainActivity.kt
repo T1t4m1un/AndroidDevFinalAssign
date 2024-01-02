1cd361d760727e08b2aabe1a4a6432fb8f3b518f
Titanium <t1t4m1un@gmail.com>
2024-01-02 17:09:27 +0800

    Basic project structure & config

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,46 +1,86 @@
 package com.example.finalproject
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.finalproject.ui.theme.FinalProjectTheme
+import kotlin.reflect.KClass
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FinalProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                App()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun App() {
+    Scaffold(
+        topBar = { MyAppBar() },
+        content = {paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) {
+                FunctionList(name = "备忘录", MemoActivity::class)
+                Divider()
+                FunctionList(name = "代办事项", TodoActivity::class)
+                Divider()
+                FunctionList(name = "记账", AccountActivity::class)
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    FinalProjectTheme {
-        Greeting("Android")
+fun MyAppBar() {
+    TopAppBar(
+        title = { Text("安卓大作业", color = Color.White) },
+        actions = {
+            IconButton(onClick = { /* 执行设置相关的动作 */ }) {
+                Icon(Icons.Filled.Settings, contentDescription = "设置")
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Black),
+    )
+}
+
+@Composable
+fun FunctionList(name: String, nextIndent: KClass<out Activity>) {
+    val context = LocalContext.current
+    val onClick = {
+        val intent = Intent(context, nextIndent.java)
+        context.startActivity(intent)
     }
+    Text(
+        text = name,
+        style = TextStyle(fontSize = 25.sp),
+        modifier = Modifier.clickable(onClick = onClick).height(50.dp)
+    )
 }
