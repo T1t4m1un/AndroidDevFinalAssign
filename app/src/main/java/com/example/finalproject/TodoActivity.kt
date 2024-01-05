791b5bf1817cd9ec4091e04c2e4d835294e6cde0
Titanium <t1t4m1un@gmail.com>
2024-01-06 03:51:36 +0800

    Implement todo notification

--- app/src/main/java/com/example/finalproject/TodoActivity.kt
+++ app/src/main/java/com/example/finalproject/TodoActivity.kt
@@ -1,13 +1,20 @@
 package com.example.finalproject
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -25,6 +32,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -41,20 +49,19 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -65,6 +72,7 @@ import androidx.navigation.compose.rememberNavController
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 
@@ -72,7 +80,30 @@ data class Todo(
     val title: String,
     val category: String,
     val datetime: String,
+    val alarmType: String = "none",
 )
+
+class AlarmReceiver(private val prompt: String = "Alarm! Wake up! Wake up!") : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, prompt, Toast.LENGTH_LONG).show()
+    }
+}
+
+private fun setSingleAlarm(context: Context, triggerTimeInMillis: Long) {
+    val intent = Intent(context, AlarmReceiver::class.java)
+    val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    alarmManager.set(AlarmManager.RTC_WAKEUP, triggerTimeInMillis, pendingIntent)
+}
+
+fun setRepeatingAlarm(context: Context, triggerTimeInMillis: Long, intervalMillis: Long) {
+    val intent = Intent(context, AlarmReceiver::class.java)
+    val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, triggerTimeInMillis, intervalMillis, pendingIntent)
+}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -240,8 +271,11 @@ fun TodoList(
         AlertDialog(
             onDismissRequest = { showDialog = false; todoToDelete = null },
             confirmButton = {
-                setTodos(todos.filter { todo: Todo -> todo != todoToDelete })
-                showDialog = false; todoToDelete = null
+                val onClick = {
+                    setTodos(todos.filter { todo: Todo -> todo != todoToDelete })
+                    showDialog = false; todoToDelete = null
+                }
+                Button(onClick = onClick) { Text("确定") }
             },
             title = { Text(text = "删除待办事项") },
             text = { Text(text = "确定要删除这个待办事项吗：${todoToDelete?.title}?") },
@@ -294,40 +328,31 @@ fun TodoScreen(todos: List<Todo>, setTodos: (List<Todo>) -> Unit, navController:
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditTodoScreen(setTodos: (List<Todo>) -> Unit, todoList: List<Todo>, idx: Int? = null, navController: NavController) {
+fun EditTodoScreen(setTodos: (List<Todo>) -> Unit, todoList: List<Todo>, idx: Int? = null, navController: NavController, context: Context) {
 
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+    val datetimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
     var title by remember { mutableStateOf(if (idx == null) "" else todoList[idx].title) }
     var category by remember { mutableStateOf(if (idx == null) "" else todoList[idx].category) }
-    val dateTime by remember {
+    var alarmType by remember { mutableStateOf("none") }
+    var datetime by remember {
         mutableStateOf(
             if (idx == null) {
                 LocalDateTime.now()
             } else {
-                LocalDateTime.parse(todoList[idx].datetime, formatter)
+                LocalDateTime.parse(todoList[idx].datetime, datetimeFormatter)
             }
         )
     }
+    var handleAlarm by remember { mutableStateOf(false) }
 
-    val datePickerState = rememberDatePickerState(selectableDates = object : SelectableDates {
-        override fun isSelectableDate(utcTimeMillis: Long): Boolean {
-            return utcTimeMillis >= System.currentTimeMillis()
-        }
-    })
-
-    val timePickerState = rememberTimePickerState(
-        initialHour = dateTime.hour,
-        initialMinute = dateTime.minute
-    )
-
-    val state = rememberScrollState()
-
+    var handlePickDatetime by remember { mutableIntStateOf(0) }
+    val scrollState = rememberScrollState()
     Scaffold(topBar = { TopAppBar(title = { Text("编辑待办") }) }) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(16.dp)
-                .verticalScroll(state)
+                .verticalScroll(scrollState)
         ) {
             OutlinedTextField(
                 value = title,
@@ -353,25 +378,61 @@ fun EditTodoScreen(setTodos: (List<Todo>) -> Unit, todoList: List<Todo>, idx: In
                 )
             )
             Spacer(modifier = Modifier.height(8.dp))
-            TimePicker(state = timePickerState, modifier = Modifier.fillMaxWidth())
+            Row(
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "Date Time Icon",
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = datetime.format(datetimeFormatter),
+                    style = MaterialTheme.typography.labelLarge,
+                    modifier = Modifier.clickable { handlePickDatetime = 2 }
+                )
+            }
             Spacer(modifier = Modifier.height(8.dp))
-            DatePicker(
-                state = datePickerState,
-                title = { Text("日期时间") },
-            )
-            // 添加其他必要的按钮或逻辑
+            Row(
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription = null,
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = when (alarmType) {
+                        "none" -> "无提醒"
+                        "single" -> "单次提醒"
+                        "repeating" -> "重复提醒"
+                        else -> "无提醒"
+                    },
+                    style = MaterialTheme.typography.labelLarge,
+                    modifier = Modifier.clickable { handleAlarm = true }
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
             Button(
                 onClick = {
-                    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-                    val monthMillis = Instant.ofEpochMilli(datePickerState.selectedDateMillis!!)
-                        .atZone(ZoneId.systemDefault())
-                        .toLocalDate()
-                        .format(formatter)
                     val newTodo = Todo(
                         title = title,
                         category = category,
-                        datetime = "$monthMillis ${timePickerState.hour}:${timePickerState.minute}"
+                        datetime = datetimeFormatter.format(datetime),
+                        alarmType = alarmType,
                     )
+                    val milli = datetime.toInstant(ZoneOffset.ofHours(8)).toEpochMilli()
+                    println(milli)
+                    when (alarmType) {
+                        "single" -> {
+                            setSingleAlarm(context, milli)
+                        }
+                        "repeating" -> {
+                            setRepeatingAlarm(context, milli, 1000 * 60 * 60) // 一小时一响
+                        }
+                    }
                     if (idx == null) {
                         setTodos(todoList + newTodo)
                     } else {
@@ -390,12 +451,89 @@ fun EditTodoScreen(setTodos: (List<Todo>) -> Unit, todoList: List<Todo>, idx: In
             }
         }
     }
+
+
+    if (handlePickDatetime == 2) {
+        val datePickerState = rememberDatePickerState()
+        AlertDialog(
+            onDismissRequest = { handlePickDatetime = 0 },
+            confirmButton = {
+                val onClick = {
+                    if (datePickerState.selectedDateMillis == null) {
+                        Toast.makeText(context, "请选择日期", Toast.LENGTH_SHORT).show()
+                    } else {
+                        datetime = Instant.ofEpochMilli(datePickerState.selectedDateMillis!!)
+                            .atZone(ZoneId.of("UTC"))
+                            .toLocalDateTime()
+                        handlePickDatetime = 1
+                    }
+                }
+                Button(onClick = onClick) { Text("确定") }
+            },
+            text = {
+                DatePicker(
+                    state = datePickerState,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        )
+    }
+    if (handlePickDatetime == 1) {
+        val timePickerState = rememberTimePickerState()
+        AlertDialog(
+            onDismissRequest = { handlePickDatetime = 0 },
+            confirmButton = {
+                Button(onClick = {
+                    datetime = datetime
+                        .plusHours(timePickerState.hour.toLong())
+                        .plusMinutes(timePickerState.minute.toLong())
+                    handlePickDatetime = 0
+                }) { Text("确定") }
+            },
+            text = {
+                TimePicker(state = timePickerState, modifier = Modifier.padding(16.dp))
+            }
+        )
+    }
+    if (handleAlarm) {
+        val alarmTypeMap = mapOf(
+            Pair("none", "无提醒"),
+            Pair("single", "单次提醒"),
+            Pair("repeating", "重复提醒"),
+        )
+        AlertDialog(
+            onDismissRequest = { handleAlarm = false },
+            confirmButton = {
+                TextButton(
+                    onClick = { handleAlarm = false },
+                ) { Text(text = "确认") }
+            },
+            text = {
+                Column {
+                    alarmTypeMap.forEach { (type, prompt) ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable { alarmType = type }
+                        ) {
+                            RadioButton(
+                                selected = alarmType == type,
+                                onClick = { /* 这里不做处理，防止重复触发 */ }
+                            )
+                            Text(text = prompt)
+                        }
+                    }
+                }
+            }
+        )
+    }
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun TodoApp() {
-    var todos = remember { mutableStateListOf<Todo>(
+fun TodoApp(context: Context) {
+    val todos = remember { mutableStateListOf<Todo>(
         Todo("安卓应用开发", "作业", "2024-01-03 18:30"),
         Todo("jyyLab", "LAB", "2024-02-09 14:43"),
         Todo("CS144", "LAB", "2024-01-03 18:30"),
@@ -412,10 +550,10 @@ fun TodoApp() {
         startDestination = "TodoHome"
     ) {
         composable("TodoHome") { TodoScreen(todos = todos, setTodos = setTodos, navController = navController) }
-        composable("NewTodo") { EditTodoScreen(setTodos = setTodos, todoList = todos, navController = navController) }
+        composable("NewTodo") { EditTodoScreen(setTodos = setTodos, todoList = todos, navController = navController, context = context) }
         composable("EditTodo/{todoId}") { backStackEntry ->
             val todoId = backStackEntry.arguments?.getString("todoId")?.toInt()
-            EditTodoScreen(setTodos = setTodos, todoList = todos, idx = todoId, navController = navController)
+            EditTodoScreen(setTodos = setTodos, todoList = todos, idx = todoId, navController = navController, context = context)
         }
     }
 }
@@ -425,7 +563,7 @@ class TodoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TodoApp()
+            TodoApp(this)
         }
     }
 }
